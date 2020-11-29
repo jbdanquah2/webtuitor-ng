a31206783c90dcf5c3450438700529d9fa309711
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2020-11-29 17:42:52 +0000

    added links to all tabs on home

--- src/app/webtuitor/nav-tabs/ebook.component.ts
+++ src/app/webtuitor/nav-tabs/ebook.component.ts
@@ -5,12 +5,12 @@ import { EbookService } from 'src/app/ebook/ebook.service';
     selector: 'ebook-tab',
     template:`
     <div class="card-group">
-        <div *ngFor="let ebook of ebooks" class="card">
+        <div [routerLink]="['/course/',ebook.id]" *ngFor="let ebook of ebooks" class="card">
         <img heigt="200" class="card-img-top" [src]="ebook.img" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{ebook.name}}</h5>
             <p class="card-text">{{ebook.description}}</p>
-            <button class="btn btn-md btn-outline-info">Get it</button>&nbsp;
+            <button [routerLink]="['/course/',ebook.id]" class="btn btn-md btn-outline-info">Get it</button>&nbsp;
             <span class="text-warning" *ngIf="ebook.license">{{ebook.license}}</span>
             <p class="card-text">
                 <span><small class="text-muted">{{ebook.published}}</small></span>&nbsp;&nbsp;&nbsp;
--- src/app/webtuitor/nav-tabs/howto-tab.component.ts
+++ src/app/webtuitor/nav-tabs/howto-tab.component.ts
@@ -5,12 +5,12 @@ import { HowtoService } from 'src/app/howto/howto.service';
     selector: 'howto-tab',
     template:`
     <div class="card-group">
-        <div *ngFor="let howto of howtos" class="card">
+        <div [routerLink]="['/course/',howto.id]" *ngFor="let howto of howtos" class="card">
         <img heigt="200" class="card-img-top" [src]="howto.img" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{howto.name}}</h5>
             <p class="card-text">{{howto.description}}</p>
-            <button class="btn btn-md btn-outline-info">Start</button>
+            <button [routerLink]="['/course/',howto.id]" class="btn btn-md btn-outline-info">Start</button>
             <p class="card-text">
                 <span><small class="text-muted">{{howto.published}}</small></span>&nbsp;&nbsp;&nbsp;
                 <span class="text-right"><small class="text-muted">{{howto.totalTime}}</small></span>
