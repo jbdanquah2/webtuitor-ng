64ca00a0aa0f08a27f81f0bee975fcdb5734ab32
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-08-08 10:27:13 +0000

    updated from ng from 13 to 14

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
 import { CookieService } from "ngx-cookie-service";
-import { expressionType } from "@angular/compiler/src/output/output_ast";
 
 @Component({
     templateUrl: 'login.component.html',
@@ -14,7 +13,7 @@ export class LoginComponent {
     userName: any
     password:any
     cookieValue: any
-    constructor(private route: Router, private authservice: AuthService, 
+    constructor(private route: Router, private authservice: AuthService,
         private cookieService:CookieService ) {
     }
 
@@ -25,18 +24,18 @@ export class LoginComponent {
         let secure = true;
         let oldDate = new Date()
         let expiry = new Date();
-        
+
         expiry.setTime(oldDate.getTime() + (30 * 60 * 1000))
         // expiry.setDate(expiry.getDate() + 1);
 
         this.authservice.loginUser(formValues.userName, formValues.password);
         this.cookieService.set('currentUser', JSON.stringify(this.authservice.currentUser), expiry, path, domain,secure,'None');
         this.cookieService.set('token', JSON.stringify(this.authservice.currentUser.password), expiry, path, domain,secure,'None');
-        
+
         this.route.navigate(['/home'])
-        
+
     }
-    
+
     cancel() {
         this.route.navigate(['/home']);
     }
