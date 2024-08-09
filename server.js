325d07715574b747088d6daebbe27238b1385084
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-08-09 23:05:46 +0000

    fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -6,8 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/webtuitor2'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/webtuitor2/index.html'));
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/webtuitor2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
